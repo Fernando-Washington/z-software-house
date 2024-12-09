08f204b012443e0e5499cebc7ad74ed38b1c0479
Fernando <ironitabarbosa@gmail.com>
2024-12-09 15:56:03 -0400

    darkmode on + custom collor styles

--- script.js
+++
@@ -1,7 +0,0 @@
-function showStack(stackType) {
-    // Esconde todas as stacks
-    document.getElementById('backend').style.display = 'none';
-    document.getElementById('frontend').style.display = 'none';
-
-    document.getElementById(stackType).style.display = 'block';
-}
