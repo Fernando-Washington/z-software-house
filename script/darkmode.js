8bc72117a4d373ebbb032a008c2d31555b119e16
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-09 13:15:21 -0400

    fix: improve dark mode

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,19 +1,30 @@
-let darkmode = localStorage.getItem('darkmode');
-        const themeSwitch = document.getElementById('theme-switch');
+const buttonClick = document.getElementById('theme-switch');
+const moonIcon = document.getElementById('moon-icon')
+const sunIcon = document.getElementById('sun-icon')
+let root = document.documentElement 
+// pega elemento raiz do HTML
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode', 'active');
+const showIcon = () => {
+    
+    if (moonIcon.style.display !=='none') {
+        root.style.setProperty("--white-color", "#F6F6F6");
+        root.style.setProperty("--background-color", "#000000");
+        root.style.setProperty("--secondary-color", "#bb3f5a");
+        root.style.setProperty("--dark-color", "#1A1A19");
+        root.style.setProperty("--moon-son-color", "#FFB400");
+
+        moonIcon.style.display = 'none'
+        sunIcon.style.display  = 'block'
+
+    } else {
+        root.style.setProperty("--white-color", "#818b9a");
+        root.style.setProperty("--background-color", "#052e43");
+        root.style.setProperty("--secondary-color", "#4c6e9f");
+        root.style.setProperty("--dark-color", "#1e1212");
+        root.style.setProperty("--moon-son-color", "#FFB400");
+
+        sunIcon.style.display  = 'none'
+        moonIcon.style.display  = 'block'
+    }
+
 }
-
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', null);
-}
-
-if (darkmode === 'active') enableDarkmode();
-
-themeSwitch.addEventListener('click', () => {
-    darkmode = localStorage.getItem('darkmode');
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
