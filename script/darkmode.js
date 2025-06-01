bef3ad870c1aa88cd484d52bf0babd603206d0a2
Fernando Washington <ironitabarbosa@gmail.com>
2025-05-31 23:19:12 -0400

    feat: add theme preference with localStorage

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,30 +1,48 @@
-const buttonClick = document.getElementById('theme-switch');
-const moonIcon = document.getElementById('moon-icon')
-const sunIcon = document.getElementById('sun-icon')
-let root = document.documentElement 
+const buttonClick = document.getElementById("theme-switch");
+const moonIcon = document.getElementById("moon-icon");
+const sunIcon = document.getElementById("sun-icon");
+let root = document.documentElement;
 // pega elemento raiz do HTML
 
-const showIcon = () => {
-    
-    if (moonIcon.style.display !=='none') {
-        root.style.setProperty("--white-color", "#F6F6F6");
-        root.style.setProperty("--background-color", "#000000");
-        root.style.setProperty("--secondary-color", "#bb3f5a");
-        root.style.setProperty("--dark-color", "#1A1A19");
-        root.style.setProperty("--moon-son-color", "#FFB400");
+window.onload = () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme == "dark") {
+    darkTheme()
+  } else {
+    lightTheme()
+  }
+};
 
-        moonIcon.style.display = 'none'
-        sunIcon.style.display  = 'block'
+function lightTheme() {
+  root.style.setProperty("--white-color", "#F6F6F6");
+  root.style.setProperty("--background-color", "#000000");
+  root.style.setProperty("--secondary-color", "#bb3f5a");
+  root.style.setProperty("--dark-color", "#1A1A19");
+  root.style.setProperty("--moon-son-color", "#FFB400");
 
-    } else {
-        root.style.setProperty("--white-color", "#818b9a");
-        root.style.setProperty("--background-color", "#052e43");
-        root.style.setProperty("--secondary-color", "#4c6e9f");
-        root.style.setProperty("--dark-color", "#1e1212");
-        root.style.setProperty("--moon-son-color", "#FFB400");
+  moonIcon.style.display = "none";
+  sunIcon.style.display = "block";
 
-        sunIcon.style.display  = 'none'
-        moonIcon.style.display  = 'block'
-    }
-
+  localStorage.setItem("theme", "light");
 }
+
+function darkTheme() {
+  root.style.setProperty("--white-color", "#818b9a");
+  root.style.setProperty("--background-color", "#052e43");
+  root.style.setProperty("--secondary-color", "#4c6e9f");
+  root.style.setProperty("--dark-color", "#1e1212");
+  root.style.setProperty("--moon-son-color", "#FFB400");
+
+  sunIcon.style.display = "none";
+  moonIcon.style.display = "block";
+
+  localStorage.setItem("theme", "dark");
+}
+
+const toggleTheme = () => {
+  if (moonIcon.style.display !== "none") {
+    lightTheme();
+  } else {
+    darkTheme();
+  }
+};
